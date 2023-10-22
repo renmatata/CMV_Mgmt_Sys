30bd34edd6cb390dea26cfa0f2bf397dc1b7b935
renmatata <126150872+renmatata@users.noreply.github.com>
2023-10-22 22:23:45 +0700

    ui_login_page

---
+++ resources/views/include/header.blade.php
@@ -0,0 +1,12 @@
+<head>
+	<title>Login V1</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/login.css') }}">    
+<!--===============================================================================================-->
+</head>
